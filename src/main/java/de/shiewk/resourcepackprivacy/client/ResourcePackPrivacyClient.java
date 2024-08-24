45afdde854f3ff86c6e8e6397ab8329a71e30f0d
Shiewk <shiewkmail@shiewk.de>
2024-08-24 13:08:03 +0200

    Show downloaded pack URLs in chat

--- src/main/java/de/shiewk/resourcepackprivacy/client/ResourcePackPrivacyClient.java
+++ src/main/java/de/shiewk/resourcepackprivacy/client/ResourcePackPrivacyClient.java
@@ -3,8 +3,6 @@ package de.shiewk.resourcepackprivacy.client;
 import de.shiewk.resourcepackprivacy.event.ChatAnnouncer;
 import de.shiewk.resourcepackprivacy.event.ScreenListener;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 
@@ -13,6 +11,6 @@ public class ResourcePackPrivacyClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ScreenEvents.AFTER_INIT.register(new ScreenListener());
-
+        ClientTickEvents.END_CLIENT_TICK.register(new ChatAnnouncer());
     }
 }
---
+++ src/main/java/de/shiewk/resourcepackprivacy/mixin/MixinServerResourcePackLoader.java
@@ -0,0 +1,25 @@
+package de.shiewk.resourcepackprivacy.mixin;
+
+import de.shiewk.resourcepackprivacy.event.ChatAnnouncer;
+import net.minecraft.client.resource.server.ServerResourcePackLoader;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.UUID;
+
+@Mixin(ServerResourcePackLoader.class)
+public class MixinServerResourcePackLoader {
+
+    @Inject(at = @At("HEAD"), method = "addResourcePack(Ljava/util/UUID;Ljava/net/URL;Ljava/lang/String;)V")
+    public void onResourcePackAdd(UUID id, URL url, String hash, CallbackInfo ci){
+        ChatAnnouncer.announce(Text.translatable("gui.resourcepackprivacy.downloading",
+                Text.literal(url.toString()))
+                .withColor(Color.ORANGE.getRGB())
+        );
+    }
+}
