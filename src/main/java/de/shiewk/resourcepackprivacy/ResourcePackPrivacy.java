a7dcdf051f1f26ee10fc8eaa5e2b08cacf0a8721
Shiewk <shiewkmail@shiewk.de>
2024-08-24 10:33:14 +0200

    Initial commit

---
+++ src/main/java/de/shiewk/resourcepackprivacy/ResourcePackPrivacy.java
@@ -0,0 +1,15 @@
+package de.shiewk.resourcepackprivacy;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourcePackPrivacy implements ModInitializer {
+
+    public static final String MOD_ID = "resourcepackprivacy";
+    public static final Logger LOGGER = LoggerFactory.getLogger(ResourcePackPrivacy.class);
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/de/shiewk/resourcepackprivacy/client/ResourcePackPrivacyClient.java
@@ -0,0 +1,10 @@
+package de.shiewk.resourcepackprivacy.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ResourcePackPrivacyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
